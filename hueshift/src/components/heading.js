5fd84c9ae936330a3901dda5667a75e788af47d1
Justin Li <your_email@example.com>
2023-01-23 12:03:43 -0800

    added timer

--- hueshift/src/components/heading.js
+++ hueshift/src/components/heading.js
@@ -1,12 +1,15 @@
 
 
-function heading() {
+function Heading() {
   return (
-    <div className="default">
-      <header className="heading">
-      </header>
-    </div>
+    <header className="heading">
+        <div className="logo">
+            <object data="path/to/image.svg" type="image/svg+xml" width="200" height="200">
+                <img src="path/to/fallback.png" alt="description of image"></img>
+            </object>
+        </div>
+    </header>
   );
 }
 
-export default App;
+export default Heading;
